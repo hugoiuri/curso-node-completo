989aea86fcec5fa6b83a268702c926c56df009d7
hugoiuri <hugoraiuri@gmail.com>
2018-12-03 11:21:11 -0200

    Tratamento de exceção e shutdown

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const pkg = require('./package.json');
 
 process.title = pkg.name;
+
+
+
+const shutdown = async () => {
+  console.info('Gracefully shutdown in progress');
+  process.exit(0);
+};
+
+process.on('SIGTERM', shutdown)
+  .on('SIGINT', shutdown)
+  .on('SIGHUP', shutdown)
+  .on('uncaughtException', err => {
+    console.error('uncaughtException caught the error: ', err);
+    throw err;
+  })
+  .on('unhandledRejection', (err, promise) => {
+    console.error(`Unhandled Rejection at: Promise ${promise} reason: ${err}`);
+    throw err;
+  })
+  .on('exit', (code) => {
+    console.info(`Node process exit with code: ${code}`);
+  });
